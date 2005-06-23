33f061749e6c02787e633ac5d63d7ededbf493b1
bwachter <bwachter>
2005-06-23 01:40:42 +0000

    fixed netaddrinfo

--- src/network.c
+++ src/network.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
+#include <ctype.h>
 #ifdef __WIN32__
 #include <windows.h>
 #include <winbase.h>
@@ -17,6 +18,45 @@
 #include "aardlog.h"
 #include "network.h"
 #include "cat.h"
+
+static struct addrinfo *
+dup_addrinfo (struct addrinfo *info, void *addr, size_t addrlen)
+{
+  struct addrinfo *ret;
+
+  ret = malloc (sizeof (struct addrinfo));
+  if (ret == NULL)
+    return NULL;
+  memcpy (ret, info, sizeof (struct addrinfo));
+  ret->ai_addr = malloc (addrlen);
+  if (ret->ai_addr == NULL)
+    {
+      free (ret);
+      return NULL;
+    }
+  memcpy (ret->ai_addr, addr, addrlen);
+  ret->ai_addrlen = addrlen;
+  return ret;
+}
+
+#if (defined __WIN32__) || (defined _BROKEN_IO)
+void
+freeaddrinfo (struct addrinfo *ai)
+{
+  struct addrinfo *next;
+
+  while (ai != NULL)
+    {
+      next = ai->ai_next;
+      if (ai->ai_canonname != NULL)
+        free (ai->ai_canonname);
+      if (ai->ai_addr != NULL)
+        free (ai->ai_addr);
+      free (ai);
+      ai = next;
+    }
+}
+#endif
 
 int netread(int sd, char *buf){
 	int i;
@@ -150,52 +190,194 @@ int netaddrinfo(const char *node, const char *service,
 #endif
 #if (defined(__WIN32__)) || (defined(_BROKEN_IO))
 #warning my getaddrinfo-emulation is currently broken. Your build should be working under Windows XP -- but nowhere else.
-		struct addrinfo **aic;
-		struct addrinfo ai;
-		struct hostent *hent;
-		struct servent *sent;
-		struct sockaddr_in saddr;
-		memset(&saddr, 0, sizeof saddr);
-		//memset(&ai, 0, sizeof(struct addrinfo));
-		aic=res; 
-		*res=0;
-		
-		if ((hent = gethostbyname(node)) == NULL) {
-			__write2("gethostbyneme() failed\n");
-			return -1;
-		}
+  struct hostent *hp;
+  struct servent *servent;
+  const char *socktype;
+  int port;
+  struct addrinfo hint, result;
+  struct addrinfo *ai, *sai, *eai;
+  char **addrs;
+  int code;
 
-		sent=malloc(sizeof(struct sockaddr_in));
-		if (!(err=strtol(service, (char **)NULL, 10))) {
-			if ((sent = getservbyname(service, NULL)) == NULL) {
-				__write2("getservbyname() failed\n");
-				return -1;
-			}
-			saddr.sin_port = sent->s_port;
-		}else
-			saddr.sin_port = htons(err);
+  memset (&result, 0, sizeof result);
 
-		memcpy(&saddr.sin_addr, hent->h_addr, sizeof(saddr.sin_addr));
-		saddr.sin_family = hent->h_addrtype;
-		//ai->ai_flags = 
-		//ai.ai_family = hent->h_addrtype;
-		ai.ai_family = AF_INET;
-		ai.ai_socktype = SOCK_STREAM;
-		ai.ai_protocol = IPPROTO_TCP;
-		ai.ai_addrlen = sizeof(saddr);
-		ai.ai_addr = (struct sockaddr *) &saddr;
-		//ai.ai_canonname = "foo";
-		ai.ai_next = (struct addrinfo*)NULL;
-		//*aic=&(ai);
-		*aic=malloc(sizeof(struct addrinfo));
-		if (!*aic) *aic=&ai; 
-		else exit(0);
-		//else (*aic)->ai_next=&ai;
-		//memcpy(&*res, &ai, sizeof(struct addrinfo));
-		//*res=&ai;
-		//netlogportservice(ai.ai_addr, ai.ai_addrlen, "Connection: ");
-		//exit (0);
-		return 0;
+  /* default for hints */
+  if (hints == NULL)
+    {
+      memset (&hint, 0, sizeof hint);
+      hint.ai_family = PF_UNSPEC;
+      hints = &hint;
+    }
+
+  /* servname must not be NULL in this implementation */
+  if (service == NULL)
+    return EAI_NONAME;
+
+  /* check for tcp or udp sockets only */
+  if (hints->ai_socktype == SOCK_STREAM)
+    socktype = "tcp";
+  else if (hints->ai_socktype == SOCK_DGRAM)
+    socktype = "udp";
+  else
+    return EAI_SERVICE;
+  result.ai_socktype = hints->ai_socktype;
+
+  /* Note: maintain port in host byte order to make debugging easier */
+  if (isdigit (*service))
+    port = strtol (service, NULL, 10);
+  else if ((servent = getservbyname (service, socktype)) != NULL)
+    port = ntohs (servent->s_port);
+  else
+    return EAI_NONAME;
+
+  /* if nodename == NULL refer to the local host for a client or any
+     for a server */
+  if (node == NULL)
+    {
+      struct sockaddr_in sin;
+
+      /* check protocol family is PF_UNSPEC or PF_INET - could try harder
+         for IPv6 but that's more code than I'm prepared to write */
+      if (hints->ai_family == PF_UNSPEC || hints->ai_family == PF_INET)
+	result.ai_family = AF_INET;
+      else
+	return EAI_FAMILY;
+
+      sin.sin_family = result.ai_family;
+      sin.sin_port = htons (port);
+      if (hints->ai_flags & AI_PASSIVE)
+        sin.sin_addr.s_addr = htonl (INADDR_ANY);
+      else
+        sin.sin_addr.s_addr = htonl (INADDR_LOOPBACK);
+      /* Duplicate result and addr and return */
+      *res = dup_addrinfo (&result, &sin, sizeof sin);
+      return (*res == NULL) ? EAI_MEMORY : 0;
+    }
+
+  /* If AI_NUMERIC is specified, use inet_addr to translate numbers and
+     dots notation. */
+  if (hints->ai_flags & AI_NUMERICHOST)
+    {
+      struct sockaddr_in sin;
+
+      /* check protocol family is PF_UNSPEC or PF_INET */
+      if (hints->ai_family == PF_UNSPEC || hints->ai_family == PF_INET)
+	result.ai_family = AF_INET;
+      else
+	return EAI_FAMILY;
+
+      sin.sin_family = result.ai_family;
+      sin.sin_port = htons (port);
+      sin.sin_addr.s_addr = inet_addr (node);
+      /* Duplicate result and addr and return */
+      *res = dup_addrinfo (&result, &sin, sizeof sin);
+      return (*res == NULL) ? EAI_MEMORY : 0;
+    }
+
+  errno = 0;
+  hp = gethostbyname (node);
+  if (hp == NULL)
+    {
+      if (errno != 0)
+	{
+	  return EAI_SYSTEM;
+	}
+      //code = h_error_ctx (&ghbnctx);
+      switch (code)
+	{
+	case HOST_NOT_FOUND: code = EAI_NODATA; break;
+	case NO_DATA: code = EAI_NODATA; break;
+#if defined(NO_ADDRESS) && NO_ADDRESS != NO_DATA
+	case NO_ADDRESS: code = EAI_NODATA; break;
+#endif
+	case NO_RECOVERY: code = EAI_FAIL; break;
+	case TRY_AGAIN: code = EAI_AGAIN; break;
+	default: code = EAI_FAIL; break;
+	}
+      return code;
+    }
+
+  /* Check that the address family is acceptable.
+   */
+  switch (hp->h_addrtype)
+    {
+    case AF_INET:
+      if (!(hints->ai_family == PF_UNSPEC || hints->ai_family == PF_INET))
+	goto eai_family;
+      break;
+    case AF_INET6:
+      if (!(hints->ai_family == PF_UNSPEC || hints->ai_family == PF_INET6))
+	goto eai_family;
+      break;
+    default:
+    eai_family:
+      return EAI_FAMILY;
+    }
+
+  /* For each element pointed to by hp, create an element in the
+     result linked list. */
+  sai = eai = NULL;
+  for (addrs = hp->h_addr_list; *addrs != NULL; addrs++)
+    {
+      struct sockaddr sa;
+      size_t addrlen;
+
+      if (hp->h_length < 1)
+        continue;
+      sa.sa_family = hp->h_addrtype;
+      switch (hp->h_addrtype)
+        {
+        case AF_INET:
+	  ((struct sockaddr_in *) &sa)->sin_port = htons (port);
+	  memcpy (&((struct sockaddr_in *) &sa)->sin_addr,
+	          *addrs, hp->h_length);
+          addrlen = sizeof (struct sockaddr_in);
+          break;
+        case AF_INET6:
+# if SIN6_LEN
+	  ((struct sockaddr_in6 *) &sa)->sin6_len = hp->h_length;
+# endif
+	  ((struct sockaddr_in6 *) &sa)->sin6_port = htons (port);
+	  memcpy (&((struct sockaddr_in6 *) &sa)->sin6_addr,
+	  	  *addrs, hp->h_length);
+          addrlen = sizeof (struct sockaddr_in6);
+          break;
+        default:
+          continue;
+        }
+
+      result.ai_family = hp->h_addrtype;
+      ai = dup_addrinfo (&result, &sa, addrlen);
+      if (ai == NULL)
+        {
+          freeaddrinfo (sai);
+          return EAI_MEMORY;
+        }
+      if (sai == NULL)
+	sai = ai;
+      else
+	eai->ai_next = ai;
+      eai = ai;
+    }
+
+  if (sai == NULL)
+    {
+      return EAI_NODATA;
+    }
+  
+  if (hints->ai_flags & AI_CANONNAME) 
+    {
+      sai->ai_canonname = malloc (strlen (hp->h_name) + 1);
+      if (sai->ai_canonname == NULL)
+        {
+          freeaddrinfo (sai);
+          return EAI_MEMORY;
+        }
+      strcpy (sai->ai_canonname, hp->h_name);
+    }
+
+  *res = sai;
+  return 0;
 #ifdef __WIN32__
 	}
 #endif
