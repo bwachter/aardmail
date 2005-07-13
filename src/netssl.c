03c764da6a74aef6aa6b555fcd83217e3db81978
bwachter <bwachter>
2005-07-13 01:08:23 +0000

    added some more comments, added some more todo

--- src/netssl.c
+++ src/netssl.c
@@ -6,7 +6,7 @@
 #include "aardmail.h"
 #include "aardlog.h"
 
-#ifdef HAVE_SSL
+#if (defined HAVE_SSL) && (!defined HAVE_MATRIXSSL)
 
 int netsslread(SSL *ssl_handle, char *buf, int len){
 	int i=0;
@@ -36,6 +36,16 @@ int netsslwrite(SSL *ssl_handle, char *buf, int len){
 	return i;
 }
 
+// in case we found a certificate we'll set it before negotiating any ssl
+// stuff. If the other site requests it on handshake SSL will just return 
+// it, if the other site requests it during normal operation SSL will give
+// us SSL_ERROR_WANT_WRITE or SSL_ERROR_WANT_READ -- which basically means
+// that OpenSSL takes care about sending the cert _now_ and wants us to
+// repeat the last operation. In case we did _not_ set a certificate 
+// OpenSSL will call the client_cert_cb()-hook if one is requested. Since 
+// we were unable to load a certifikate on startup we guess we still would
+// not get it, and use this method just as a simple way to report a 
+// certificate problem 
 static int provide_client_cert(SSL *ssl, X509 **cert, EVP_PKEY **pkey)
 {
 	(void)ssl;
