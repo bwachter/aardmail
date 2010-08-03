59d65872a067f15e39777ea49597c8cf3d7bd32b
Bernd Wachter <bwachter-aardmail@lart.info>
2010-08-03 20:21:33 +0300

    add identity support to sendmail

--- src/sendmail.c
+++ src/sendmail.c
@@ -31,80 +31,89 @@
 char *uniqname;
 
 int main(int argc, char **argv){
-	int smcfg=0;
-	int i=1, isbody=0, c;
-	char buf[1024], from[1024], *mymaildir=NULL;
+  int smcfg=0;
+  int i=1, isbody=0, c;
+  char buf[1024], from[1024], *mymaildir=NULL;
+  char *identity=NULL;
 #if (defined(__WIN32__)) || (defined _BROKEN_IO)
-	FILE *fd;
+  FILE *fd;
 #else
-	int fd;
+  int fd;
 #endif
 
-	while ((c=getopt(argc, argv, "d:f:F:o:tvV")) != EOF){
-		switch(c){
-		case 'd':
-			loglevel(atoi(optarg));
-			break;
-		case 'f': // set the from: line. will be overwritten if there's a From:-line
-			break;
-		case 'F':
-			break; // set the fullname 
-		case 'o':
-			if (optarg[0]=='i') smcfg|=AM_SM_IGNORE_DOTS; // ignore single dots
-			break;
-		case 't':
-			//FIXME
-			break;
-		case 'v':
-			loglevel(4);
-			break;
-		case 'V':
-			__write1(cati("aardmail-sendmail ", AM_VERSION, "\n", NULL));
-			exit(0);
-		}
-	}
+  while ((c=getopt(argc, argv, "a:d:f:F:o:tvV")) != EOF){
+    switch(c){
+      case 'a':
+        identity=strdup(optarg);
+        break;
+      case 'd':
+        loglevel(atoi(optarg));
+        break;
+      case 'f': // set the from: line. will be overwritten if there's a From:-line
+        break;
+      case 'F':
+        break; // set the fullname
+      case 'o':
+        if (optarg[0]=='i') smcfg|=AM_SM_IGNORE_DOTS; // ignore single dots
+        break;
+      case 't':
+        //FIXME
+        break;
+      case 'v':
+        loglevel(4);
+        break;
+      case 'V':
+        __write1(cati("aardmail-sendmail ", AM_VERSION, "\n", NULL));
+        exit(0);
+    }
+  }
 
-	if (mdfind(NULL)){
-		logmsg(L_ERROR, F_MAILDIR, "unable to find maildir", NULL);
-		return -1;
-	}
-	cat (&mymaildir, maildirpath, "/.spool", NULL);
-	fd=mdopen(mymaildir, &uniqname);
+  if (mdfind(NULL)){
+    logmsg(L_ERROR, F_MAILDIR, "unable to find maildir", NULL);
+    return -1;
+  }
+
+  if (identity)
+    cat (&mymaildir, maildirpath, "/.spool/", identity, NULL);
+  else
+    cat (&mymaildir, maildirpath, "/.spool", NULL);
+
+  fd=mdopen(mymaildir, &uniqname);
 #if (defined(__WIN32)) || (defined _BROKEN_IO)
-	if (fd == NULL)
+  if (fd == NULL)
 
 #else
-	if (fd == -1)
+    if (fd == -1)
 #endif
-		{
-			logmsg(L_ERROR, F_GENERAL, "opening spool failed", NULL);
-			return -1;
-		}
-	while (i>0){
-		i=read(0, buf, 1024);
-		if (!isbody){
-			if (!strncasecmp(buf, "from:", 5)){
-				logmsg(L_DEBUG, F_GENERAL, "Found from: ", buf, NULL);
-			}
-			if (!strncmp(buf, "\n", 1)) {
-				isbody=1;
-				if (!strncmp(from, "\0", 1)){
-					logmsg(L_ERROR, F_GENERAL, "Unable to figure out From:", NULL);
-					goto error;
-				}
-				// header time ;)
-			}
-		}
-		if (!strncmp(buf+i-2, "\r\n", 2)) {
-			logmsg(L_ERROR, F_GENERAL, "Having \\r\\n in the input is bad for your karma", NULL);
-			goto error;
-		}
-		if (!strncmp(buf+i-2, ".\n", 2) && !(smcfg & AM_SM_IGNORE_DOTS)) break;
-		filewrite(fd, buf, i);
-	}
-	return mdclose(mymaildir, &uniqname, fd);
- error:
-	logmsg(L_ERROR, F_GENERAL, "some stupid error occured", NULL);
-	return -1;
-}
+    {
+      logmsg(L_ERROR, F_GENERAL, "opening spool failed", NULL);
+      return -1;
+    }
+  while (i>0){
+    i=read(0, buf, 1024);
+    if (!isbody){
+      if (!strncasecmp(buf, "from:", 5)){
+        logmsg(L_DEBUG, F_GENERAL, "Found from: ", buf, NULL);
+      }
+      if (!strncmp(buf, "\n", 1)) {
+        isbody=1;
+        if (!strncmp(from, "\0", 1)){
+          logmsg(L_ERROR, F_GENERAL, "Unable to figure out From:", NULL);
+          goto error;
+        }
+        // header time ;)
+      }
+    }
+    if (!strncmp(buf+i-2, "\r\n", 2)) {
+      logmsg(L_ERROR, F_GENERAL, "Having \\r\\n in the input is bad for your karma", NULL);
+      goto error;
+    }
+    if (!strncmp(buf+i-2, ".\n", 2) && !(smcfg & AM_SM_IGNORE_DOTS)) break;
+    filewrite(fd, buf, i);
+  }
+  return mdclose(mymaildir, &uniqname, fd);
 
+  error:
+  logmsg(L_ERROR, F_GENERAL, "some stupid error occured", NULL);
+  return -1;
+}
